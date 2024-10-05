c0c764422884cf182f302d726d1a7d08059502f7
Carlos Patino <carlospatinos@gmail.com>
2024-10-05 05:46:21 +0100

    Adding a second job in github actions to build both springboot proyects

--- stockservice/src/test/java/com/carlospatinos/stockservice/StockServiceApplicationTests.java
+++ stockservice/src/test/java/com/carlospatinos/stockservice/StockServiceApplicationTests.java
@@ -1,14 +1,14 @@
 package com.carlospatinos.stockservice;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.kafka.KafkaAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.test.context.EmbeddedKafka;
 
 @SpringBootTest
 // @SpringBootTest("spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.kafka.KafkaAutoConfiguration")
-@EnableAutoConfiguration(exclude = { KafkaAutoConfiguration.class })
-
+// @EnableAutoConfiguration(exclude = { KafkaAutoConfiguration.class })
+@EmbeddedKafka(partitions = 1, brokerProperties = {
+		"listeners=PLAINTEXT://localhost:9092", "port=9092" })
 class StockServiceApplicationTests {
 
 	@Test
